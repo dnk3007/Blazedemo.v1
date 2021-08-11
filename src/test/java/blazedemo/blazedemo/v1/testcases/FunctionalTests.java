ecc4a179ed3e007342abc01cf0aa5b1de7f55a54
Naveen Kumar D <dnk3007@gmail.com>
2021-08-11 22:26:20 +0530

    IdChanges

--- src/test/java/blazedemo/blazedemo/v1/testcases/FunctionalTests.java
+++ src/test/java/blazedemo/blazedemo/v1/testcases/FunctionalTests.java
@@ -9,33 +9,38 @@ public class FunctionalTests extends BaseClass {
 
 	@Test
 	public void checkThePriceBeforeAndAfterFlighSelection() {
+		try {
 		driver.get(baseURL);
-		Select selectfrom = new Select(driver.findElement(By.name("fromPort")));
-		selectfrom.selectByValue("Mexico City");
-		Select selectTo = new Select(driver.findElement(By.name("toPort")));
-		selectTo.selectByValue("Berlin");
+		Select selFrom = new Select(driver.findElement(By.name("fromPort")));
+		selFrom.selectByValue("Mexico City");
+		Select selTo = new Select(driver.findElement(By.name("toPort")));
+		selTo.selectByValue("Berlin");
 		driver.findElement(By.xpath("//input[@type='submit']")).click();
-		String xpathChooseFlight = "//form[@name=\'" + "VA43" + "\']//parent::tr//td/input";
-		String expectedPrice = driver.findElement(By.xpath("//form[@name='VA43']//parent::tr//td[6]")).getText().substring(1);
-		driver.findElement(By.xpath(xpathChooseFlight)).click();
+		
+		String expPrice = driver.findElement(By.xpath("//form[@name='VA43']//parent::tr//td[6]")).getText().substring(1);
+		String chooseFlight = "//form[@name=\'" + "VA43" + "\']//parent::tr//td/input";
+		driver.findElement(By.xpath(chooseFlight)).click();
 		String str[]=driver.findElement(By.xpath("//p[contains(text(),'Price')]")).getText().split(":");
 		
-		Assert.assertEquals(str[1],expectedPrice);//Comparing both the prices before and after selcting flight
+		Assert.assertEquals(str[1],expPrice);//Comparing both the prices before and after selecting flight
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 	
-	@Test(groups = "Sanity")
+	@Test
 	public void checkAllAvailableAirlinesDisplayedonReservePage() {
 		driver.get(baseURL);
-		Select selectfrom = new Select(driver.findElement(By.name("fromPort")));
-		selectfrom.selectByValue("Mexico City");
-		Select selectTo = new Select(driver.findElement(By.name("toPort")));
-		selectTo.selectByValue("Berlin");
+		Select selFrom = new Select(driver.findElement(By.name("fromPort")));
+		selFrom.selectByValue("Mexico City");
+		Select selTo = new Select(driver.findElement(By.name("toPort")));
+		selTo.selectByValue("Berlin");
 		driver.findElement(By.xpath("//input[@type='submit']")).click();
-		Assert.assertTrue(driver.findElement(By.xpath("//td[contains(text(),'Virgin America')]")).isDisplayed());
-		Assert.assertTrue(driver.findElement(By.xpath("//td[contains(text(),'United Airlines')]")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//td[contains(text(),'Virgin America')]")).isDisplayed());		
 		Assert.assertTrue(driver.findElement(By.xpath("//td[contains(text(),'Aer Lingus')]")).isDisplayed());
 		Assert.assertTrue(driver.findElement(By.xpath("//td[contains(text(),'Lufthansa')]")).isDisplayed());
-		
+		Assert.assertTrue(driver.findElement(By.xpath("//td[contains(text(),'United Airlines')]")).isDisplayed());
 		
 	}
 }
